99bdcc26da425099eba44d96852d0c179cec09fb
Elizaveta <lisasorokina990@gmail.com>
2025-10-27 21:33:23 -0400

    Initial Commit

---
+++ TravelGenie/TravelGenieApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelGenieApp.swift
+//  TravelGenie
+//
+//  Created by Elizaveta Sorokina on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct TravelGenieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TravelGenieTests/TravelGenieTests.swift
@@ -0,0 +1,17 @@
+//
+//  TravelGenieTests.swift
+//  TravelGenieTests
+//
+//  Created by Elizaveta Sorokina on 10/27/25.
+//
+
+import Testing
+@testable import TravelGenie
+
+struct TravelGenieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
